265d8509cdd04f3dd898c2c3b666e4474128e7ee
krishnasarathi <krishnasarathishem@gmail.com>
2025-07-23 14:58:36 +0530

    fix appwriteClient.js

--- src/appwriteClient.js
+++ src/appwriteClient.js
@@ -9,13 +9,4 @@ client
 const account = new Account(client);
 const databases = new Databases(client);
 
-// Debug: Log to ensure client is configured properly
-console.log('Appwrite client configured:', {
-  endpoint: client.config.endpoint,
-  project: client.config.project
-});
-
-// Debug: Log available account methods
-console.log('Available account methods:', Object.getOwnPropertyNames(Object.getPrototypeOf(account)));
-
 export { client, account, databases };
