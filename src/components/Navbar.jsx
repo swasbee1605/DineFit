8eca886742329bb855af50d2ba78af479a48a856
krishnasarathi <krishnasarathishem@gmail.com>
2025-07-23 13:23:40 +0530

    fix navbar style and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,17 @@ const Navbar = () => {
   return (
     <nav className="bg-green-600 text-white px-6 py-4 shadow-md flex items-center justify-between">
       <h1 className="text-lg font-semibold">DineFit</h1>
-      <NavLink to="/" className="ml-4 hover:underline">
+      <span className="flex space-x-4">
+        <NavLink to="/" className="ml-4 hover:underline">
         Home
-      </NavLink>
-      <NavLink to="/login" className="ml-4 hover:underline">
-        Login
-      </NavLink>
-      <NavLink to="/signup" className="ml-4 hover:underline">
-        Signup
-      </NavLink>
+        </NavLink>
+        <NavLink to="/login" className="ml-4 hover:underline">
+            Login
+        </NavLink>
+        <NavLink to="/signup" className="ml-4 hover:underline">
+            Signup
+        </NavLink>
+      </span>
     </nav>
   )
 }
