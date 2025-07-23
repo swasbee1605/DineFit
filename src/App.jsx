8905fe13d76250a7331df836d0bbc5a3e1286f4b
krishnasarathi <krishnasarathishem@gmail.com>
2025-07-23 13:32:24 +0530

    update global font family to roboto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import './index.css'
 
 function App() {
   return (
